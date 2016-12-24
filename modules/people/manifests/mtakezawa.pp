92e1171782bafe7f7561c1aac3f9b73c7245fa57
Makoto Takezawa <makoto@takezawa.biz>
2016-12-24 22:22:58 +0900

    update IntelliJ.
        IntelliJ-IDEA-IU-2016.3.1

--- modules/people/manifests/mtakezawa.pp
+++ modules/people/manifests/mtakezawa.pp
@@ -217,7 +217,7 @@ class people::mtakezawa {
     # intellij
     class { 'intellij':
         edition => 'ultimate',
-        version => '2016.1.3'
+        version => '2016.3.1'
     }
 
     #include vmware_fusion
